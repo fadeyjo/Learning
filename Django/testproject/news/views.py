44ee7667de853fe0da8376920172dbefc76e5ef3
fadeyjo <komrad.gubi2017@yandex.ru>
2024-06-15 20:12:39 +0300

    Dinamyc pages

--- Django/testproject/news/views.py
+++ Django/testproject/news/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Article
 from .forms import ArticleForm
+from django.views.generic import DetailView
 
 
 def news_home(request):
     news = Article.objects.order_by('title')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Article
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
 
 
 def create(request):
