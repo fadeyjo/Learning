457ee6a47fa9eddf12c2f36267801cf2a0df5ded
fadeyjo <komrad.gubi2017@yandex.ru>
2024-06-15 12:18:42 +0300

    Filters and args in templates

--- Django/testproject/main/views.py
+++ Django/testproject/main/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello']
+    }
+    return render(request, 'main/index.html', data)
 
 
 def about(request):
